570bd9edac4170db703fc7d012812bf42ff05fa2
Andree Panjaitan <andreepanjaitan@Andrees-MacBook-Pro.local>
2023-09-27 18:11:57 +0700

    initial project

---
+++ src/main/java/com/ama/imanuel/connectwithmebe/ConnectWithMeBeApplication.java
@@ -0,0 +1,13 @@
+package com.ama.imanuel.connectwithmebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectWithMeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectWithMeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ama/imanuel/connectwithmebe/ConnectWithMeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ama.imanuel.connectwithmebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectWithMeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
